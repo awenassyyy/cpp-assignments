7c74dfc8c90c36e8a8abac7bb9d24a84a22c12d4
awenassyyy <080bct005@ioepc.edu.np>
2024-08-01 16:40:05 +0545

    fix spelling error

--- problem3.cpp
+++ problem3.cpp
@@ -1,5 +1,5 @@
 /*
-Define a class Library with bookTitle, authro, and numberOfCopies as data
+Define a class Library with bookTitle, author, and numberOfCopies as data
 members. Implement methods to add new books, issue books, and return books.
 Ensure that the number of copies is appropriately updated for each operation.
 */
